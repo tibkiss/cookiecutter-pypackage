9d2fb2cff4eb84a2fcd5afcea0efa4c05d58972a
Gabor Lekeny <glekeny@hortonworks.com>
2018-06-18 07:12:08 +0200

    SUST-677 Use pbr for versioning
    Change-Id: I0c99fa67bfc5c6da972f75033a3ccc9c628986d1

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,7 +1,8 @@
-# -*- coding: utf-8 -*-
-
 """Top-level package for {{ cookiecutter.project_name }}."""
+import pbr.version
 
-__author__ = """{{ cookiecutter.full_name }}"""
+__author__ = '{{ cookiecutter.full_name }}'
 __email__ = '{{ cookiecutter.email }}'
-__version__ = '{{ cookiecutter.version }}'
+
+version_info = pbr.version.VersionInfo('{{ cookiecutter.project_slug }}')
+__version__ = version_info.release_string()
